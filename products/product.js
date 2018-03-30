e9e02ae3dfd394ef6f6e379a1c4797cae6a1309b
TarunJuluru <tarun.design99@gmail.com>
2018-03-30 09:46:03 +0530

    miner cahnges

--- products/product.js
+++ products/product.js
@@ -9,11 +9,13 @@ function viewer(imagesourse) {
 function cartIns() {
     cartNumber = cartNumber + 1;
     document.getElementById("result").innerHTML = cartNumber;
-    document.getElementById("add-cart").innerHTML = cartNumber;
 }
 
 function cartDes() {
     cartNumber = cartNumber - 1;
     document.getElementById("result").innerHTML = cartNumber;
+}
+
+function cartResult() {
     document.getElementById("add-cart").innerHTML = cartNumber;
 }
